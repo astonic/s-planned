9f50497c74f96d8d689e6543ba744d46f51144d0
Aston Motsau <astonmotsau@Astons-MacBook-Air.local>
2026-04-26 14:43:52 +0200

    chore: add project config files (package.json, tsconfig, next.config, tailwind)

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: [
+    '@fluentui/react-components',
+    '@fluentui/react-icons',
+    '@griffel/react',
+  ],
+}
+
+module.exports = nextConfig
